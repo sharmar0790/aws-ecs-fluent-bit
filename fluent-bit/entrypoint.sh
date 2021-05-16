28baa579c6f157ff5d168657653729db593b10e0
RV0718 <ravi.sharma0790@gmail.com>
2021-05-16 13:50:58 +0100

    Added conf to push the logs to cw

--- fluent-bit/entrypoint.sh
+++ fluent-bit/entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "BUCKET_NAME >>>  " $BUCKET_NAME_ENV
+echo "BUCKET_NAME >>>  " $BUCKET_NAME
 echo "CUSTOMER_FOLDER >>>  " $CUSTOMER_FOLDER
 
 rm -rf /fluent-bit/etc/
 aws s3 cp --recursive  s3://$BUCKET_NAME/$CUSTOMER_FOLDER /fluent-bit/etc/
 
 echo "Listing file in /fluent-bit/etc/ >>> "
-ls -l /fluent-bit/etc/
+#ls -l /fluent-bit/etc/
 echo -n "AWS for Fluent Bit Container Image Version >>> "
 cat /AWS_FOR_FLUENT_BIT_VERSION
 
 echo -n "AWS Version >>> "
 aws --version
 
-exec /fluent-bit/bin/fluent-bit -c /fluent-bit/etc/fluent-bit.conf
+exec /fluent-bit/bin/fluent-bit -e /fluent-bit/cloudwatch.so  -c /fluent-bit/etc/fluent-bit.conf
